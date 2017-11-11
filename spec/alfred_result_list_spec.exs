69f34b9d45ab382f10b1c4dfe2696dfc0458916b
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2017-11-11 09:51:10 -0800

    Rewrite expectations without pipes

--- spec/alfred_result_list_spec.exs
+++ spec/alfred_result_list_spec.exs
@@ -10,27 +10,27 @@ defmodule Alfred.ResultList.Spec do
     it "creates an empty list" do
       list = ResultList.new
 
-      expect list.items |> to(have_length 0)
-      expect list.variables |> to(have_length 0)
+      expect(list.items).to(have_length 0)
+      expect(list.variables).to(have_length 0)
     end
 
     it "creates a list when given only a single item" do
       list = ResultList.new(Result.new("title", "subtitle"))
 
-      expect list.items |> to(have_length 1)
-      expect list.variables |> to(have_length 0)
+      expect(list.items).to(have_length 1)
+      expect(list.variables).to(have_length 0)
     end
 
     it "throws an error when given something that isn't a Result" do
-      expect fn -> ResultList.new(5) end |> to(raise_exception FunctionClauseError)
+      expect(fn -> ResultList.new(5) end).to(raise_exception FunctionClauseError)
     end
 
     it "creates a list of Result items" do
       item = Result.new("title", "subtitle")
       list = ResultList.new([item, item, item])
 
-      expect list.items |> to(have_length 3)
-      expect list.variables |> to(have_length 0)
+      expect(list.items).to(have_length 3)
+      expect(list.variables).to(have_length 0)
     end
 
     it "creates a list with variables" do
@@ -38,8 +38,8 @@ defmodule Alfred.ResultList.Spec do
       variables = %{"foo": "bar", "bar": "baz", "baz": "quux"}
       list = ResultList.new([item, item, item], variables: variables)
 
-      expect list.items |> to(have_length 3)
-      expect list.variables |> to(have_length 3)
+      expect(list.items).to(have_length 3)
+      expect(list.variables).to(have_length 3)
     end
 
     it "creates a list with a rerun value" do
@@ -47,22 +47,22 @@ defmodule Alfred.ResultList.Spec do
       variables = %{foo: "bar"}
       list = ResultList.new([item, item, item], variables: variables, rerun: 3.0)
 
-      expect list.items |> to(have_length 3)
-      expect list.variables |> to(have_length 1)
-      expect list.rerun |> to(eq 3.0)
+      expect(list.items).to(have_length 3)
+      expect(list.variables).to(have_length 1)
+      expect(list.rerun).to(eq 3.0)
     end
 
     it "throws an error if the rerun value is not a number" do
       item = Result.new("title", "subtitle")
 
-      expect fn -> ResultList.new([item], variables: %{}, rerun: "foo") end |> to(raise_exception ArgumentError)
+      expect(fn -> ResultList.new([item], variables: %{}, rerun: "foo") end).to(raise_exception ArgumentError)
     end
 
     it "throws an error if the rerun value is less than 1.0 or greater than 5.0" do
       item = Result.new("title", "subtitle")
 
-      expect fn -> ResultList.new([item], variables: %{}, rerun: -1.0) end |> to(raise_exception ArgumentError)
-      expect fn -> ResultList.new([item], variables: %{}, rerun: 10.0) end |> to(raise_exception ArgumentError)
+      expect(fn -> ResultList.new([item], variables: %{}, rerun: -1.0) end).to(raise_exception ArgumentError)
+      expect(fn -> ResultList.new([item], variables: %{}, rerun: 10.0) end).to(raise_exception ArgumentError)
     end
   end
 
@@ -73,7 +73,7 @@ defmodule Alfred.ResultList.Spec do
       list = ResultList.new([item, item, item], variables: variables, rerun: 3.0)
       {:ok, json} = ResultList.to_json(list)
 
-      expect json |> to(eq fixture("results-with-variables-and-rerun.txt"))
+      expect(json).to(eq fixture("results-with-variables-and-rerun.txt"))
     end
 
     it "does not emit the variables key when no variables are present" do
@@ -81,7 +81,7 @@ defmodule Alfred.ResultList.Spec do
       list = ResultList.new([item, item, item])
       {:ok, json} = ResultList.to_json(list)
 
-      expect json |> to(eq fixture("multiple-results.txt"))
+      expect(json).to(eq fixture("multiple-results.txt"))
     end
 
     it "does not emit the rerun key when no rerun is present" do
@@ -90,7 +90,7 @@ defmodule Alfred.ResultList.Spec do
       list = ResultList.new([item, item, item], variables: variables)
       {:ok, json} = ResultList.to_json(list)
 
-      expect json |> to(eq fixture("results-with-variables.txt"))
+      expect(json).to(eq fixture("results-with-variables.txt"))
     end
   end
 end
